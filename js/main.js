e7218723f8577b873c575afb6173521990477c70
Acedev003 <acedeveloper003@gmail.com>
2022-03-27 21:50:04 +0530

    Added comments and refactored code

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//Reference all html elements
 let canvas       = document.getElementById("canvas");
 let clear_button = document.getElementById("clear_canvas");
 let eval_button  = document.getElementById("guess_digit");
@@ -7,30 +8,34 @@ let view_probs   = document.getElementById("display_probs");
 let stat_modal   = document.getElementById("stat_modal");
 let chart_div    = document.getElementById("chart_div");
 
+//Load canvas library (jSketch) and set default config
 let sketcher = new Sketchable(canvas,{graphics: {lineWidth: 10,strokeStyle: '#C2D991', fillStyle: '#C2D991'}});
 
-let default_chartdata = [
-                            ['Digit', 'Probability'],
-                            ['0',0],
-                            ['1',0],
-                            ['2',0],
-                            ['3',0],
-                            ['4',0],
-                            ['5',0],
-                            ['6',0],
-                            ['7',0],
-                            ['8',0],
-                            ['9',0]
-                        ];
 
-let variable_data = default_chartdata;
-
+// Hide view probability button 
 view_probs.style.visibility = 'hidden';
+
+//Default chart data
+let data = [
+    ['Digit', 'Probability'],
+    ['0',0],
+    ['1',0],
+    ['2',0],
+    ['3',0],
+    ['4',0],
+    ['5',0],
+    ['6',0],
+    ['7',0],
+    ['8',0],
+    ['9',0]
+];
+
+//Expand modal and draw chart on button (view_probs) click
 view_probs.onclick = () => 
 {
     let drawChart = () =>
     {
-        let chart_data =  google.visualization.arrayToDataTable(variable_data);
+        let chart_data =  google.visualization.arrayToDataTable(data);
         let options = 
           {
             title:'Prediction Probabilities for each digit',
@@ -55,6 +60,7 @@ view_probs.onclick = () =>
 };
 
 
+//Reset canvas and hide view probability button
 clear_button.onclick = () => 
 {
     sketcher.clear();
@@ -63,14 +69,13 @@ clear_button.onclick = () =>
 
     view_probs.style.visibility = 'hidden';
     view_probs.classList.remove("fadeIn");
-
-    variable_data = default_chartdata;
 };
 
 
 let model_loaded = false;
 let model = null;
 
+//Load tensorflow model
 async function loadtf()
 {   
     model = await tf.loadLayersModel('model/model.json');
@@ -93,14 +98,10 @@ eval_button.onclick = () =>
 
     image = tf.browser.fromPixels(canvas,1).resizeBilinear([28,28]);
 
-    //preview_image = tf.cast(image,'int32');
-    //tf.browser.toPixels(preview_image,canvas);
-    
-    image = tf.div(image,255);
-    image = image.expandDims();
-    preds = model.predict(image);
+    image = tf.div(image,255);     //Normalize
+    image = image.expandDims();    //expandDims to match model input shape
+    preds = model.predict(image);  //run prediction
 
-    let output = -1;
     preds.data().then((scores) =>
     {
         console.log(scores);
@@ -108,12 +109,12 @@ eval_button.onclick = () =>
         guess = scores.indexOf(Math.max(...scores));
         prob  = scores[guess];
         
-        quote.innerHTML = ` I am ${(prob*100).toFixed(2)}% certain that the digit you drew somewhat looked like ... `;
+        quote.innerHTML = ` I am ${(prob*100).toFixed(2)}% certain that the digit you drew somewhat looks like ... `;
         output_block.innerText = `${guess}`;
         
         for(let i = 1; i < 11; i++)
         {
-            variable_data[i][1] = Number( scores[i-1].toFixed(2));
+            data[i][1] = Number( scores[i-1].toFixed(2));
         }
 
         view_probs.style.visibility = 'visible';
